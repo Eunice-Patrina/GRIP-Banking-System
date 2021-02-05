1ef12fb66d0a66c5d32cd4cf84ce11770932dbc5
Eunice27112000 <43789261+Eunice27112000@users.noreply.github.com>
2021-02-05 21:07:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang=en>
+    <head>
+        <title>Banking System:Homepage</title>
+        <!-- linking javascript file, bootstrap and css-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href ="styles.css">
+    </head>
+	<body id="home">
+	<!-- navbar -->
+		
+		<nav class="navbar" style="color:white;">
+			<h2>The Sparks Foundation</h2>
+		</nav>
+		
+		<!-- main -->
+		<h2 class="head" style="font: small-caps bold 30px/1 sans-serif;"> Homepage</h2>
+		<h1 class="fw-normal head" style="text-align:center;font: italic small-caps bold 36px/2 cursive;">Banking System</h1>
+		
+		<main class="container p-5">
+			<a href="customers.php" ><button class="btn btn-dark btn-lg">View all customer</button></a>
+			<a href="history.php" ><button class="btn btn-dark btn-lg" type="submit">History</button></a>
+		</main>
+		
+		
+		<footer class="small text-center text-muted">
+            Banking System by Eunice Patrina
+        </footer>
+	</body>
+<html>
